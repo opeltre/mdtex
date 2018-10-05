4569e522ae8aa463973d3049e916e40039d7a50b
Olivier Peltre <opeltre@gmail.com>
2018-10-05 12:35:47 +0200

    fix bundler.bash

--- bundler.bash
+++ bundler.bash
@@ -8,11 +8,11 @@ bundle='bundle.js'
 b=$(pwd)/$bundle
 
 echo '/* mdtex bundle */' > $b && echo "bundling..."
-echo 'function mdtex(text) { return parse(text); ' > $b
+echo 'function mdtex(text) { return parse(text); ' >> $b
 
 for d in ${deps[*]}
 do  
     cat $(pwd)/$d.js >> $b && echo "->  $d.js"
 done
 
-echo '}' > $b   && echo "...done"
+echo '}' >> $b   && echo "...done"
